3b194f784de6af5c7c5c2f9c7fa38f6864b1202e
Red-French <red@redfrench.net>
2017-02-27 23:19:19 -0600

    initial commit

---
+++ 03 - CSS Variables/README.md
@@ -0,0 +1,2 @@
+# CSS Variables
+CSS can use variables that are updated on the fly via good ol' JavaScript.
---
+++ 03 - CSS Variables/scripts.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";  // dataset is an object that will contain all the data attributes from the specific element
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
